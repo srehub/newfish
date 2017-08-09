b672da5bff1d3abee4040181883ecbdeed8224e5
serven <china_serven@163.com>
2017-08-10 00:57:54 +0800

    demo

---
+++ chinaserven/test.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python
+#!coding:utf-8
+
+def func(n):
+    sum = 0
+    if n <= 0:
+        return 1
+    else:
+        return n * func(n - 1)
+
+print "Now, the total is: ", (func(5))
+
+print 123
