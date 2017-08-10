bd1b6dd1dbe0884f73a7cdd7dfa5a9526fd61e2d
cateleon <yx.leon@qq.com>
2017-08-11 03:42:43 +0800

    stack-p1

---
+++ cateleon/2017w32/2017w32d04-demo-stack-01.py
@@ -0,0 +1,41 @@
+#!/usr/bin/python
+# encoding:utf-8
+
+"""
+@author: yuanxin
+contact:
+@file: 2017/8/11-demo-stack-01.py
+@time: 2017/8/11 
+"""
+
+
+list = [ ]
+num = 6
+for i in range(1,9):
+    # print(i)
+    if i < num:
+        list.append(i)
+        print( (list), ", I want" , (num),  "balls, Now there is ", len(list), ", give me one more")
+    elif i == num:
+        list.append(i)
+        print( (list) , ", That's right, I have ", len(list), "balls, great !" )
+    else:
+        list.append(i)
+        print((list), "There are ", len(list) , "balls, it's too many balls, i will drop", i)
+        list.pop()
+        print("Still ,i have " ,(list))
+
+# # print(i)
+    # print.len(i)    # if i <= 7:
+    # pass   # i.pop()
+    # print(i)
+    # i.pop()
+    # print(i)
+
+# from collections import deque
+# que = deque([ 'a','b','c','d','e','f' ])
+# que.append('g')
+# que.popleft()                #  a
+# print(que)
+# que.popleft()                #  b
+# print(que)
