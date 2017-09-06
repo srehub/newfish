075a38d49c38f8856d3faf346cf177b9a3fab8d6
cateleon <yx.leon@qq.com>
2017-09-07 00:12:29 +0800

    sophia

---
+++ cateleon/2017w36/boaz.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python
+# encoding:utf-8
+
+"""
+@author: yuanxin
+contact:
+@file: 2017/9/6-boaz.py
+@time: 2017/9/6 
+"""
+
+class genes:
+    def __init__(self):
+        print("I am mom!")
+
+boza=genes()
